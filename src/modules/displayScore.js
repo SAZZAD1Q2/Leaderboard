3ee251522f5c32d68da3c8d96b26729b42309e1a
SAZZAD1Q2 <kurulia115@gmail.com>
2023-04-29 13:07:36 +0600

    displayScore.js and style.css modified

--- src/modules/displayScore.js
+++ src/modules/displayScore.js
@@ -1,11 +1,19 @@
-const displayScores = (scores) => {
-  const ul = document.querySelector('.displayScore');
-  ul.innerHTML = '';
+const scoreList = document.getElementById('scoreList');
 
-  scores.forEach((user) => {
-    const li = document.createElement('li');
-    li.textContent = `${user.user}: ${user.score}`;
-    ul.appendChild(li);
+const displayScores = (scores) => {
+  scoreList.innerHTML = ''; // Clear the previous scores
+  scores.forEach((score, index) => {
+    const row = document.createElement('tr');
+    const nameCol = document.createElement('td');
+    nameCol.textContent = score.user;
+    const scoreCol = document.createElement('td');
+    scoreCol.textContent = score.score;
+    row.appendChild(nameCol);
+    row.appendChild(scoreCol);
+    scoreList.appendChild(row);
+
+    // Change the background color of the row using inline CSS
+    row.style.backgroundColor = index % 2 === 0 ? '#e6e6e6' : '#f2f2f2';
   });
 };
 
